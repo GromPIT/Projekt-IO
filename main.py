326695f43a9c401ec530400f4fcc2f6b87eeb428
Piter <piter@nowhere.net>
2023-01-16 23:58:25 +0100

    dodanie generacji liczb losowych

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+import random
+
+ZAKRES_MIN = 0
+ZAKRES_MAX = 100
+ILE_LICZB = 100
+
+def generujLiczbyLosowe():
+    listaLiczb = []
+
+    for i in range(ILE_LICZB):
+        listaLiczb.append(random.randint(ZAKRES_MIN, ZAKRES_MAX))
+
+    return listaLiczb
+
 def main():
-    print("Hello world")
+    listaLiczb = generujLiczbyLosowe()
+    print(listaLiczb)
+
     return
 
 if __name__ == "__main__":
     main()
-    #test
-    #test
