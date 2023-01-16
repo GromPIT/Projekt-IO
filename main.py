b525b6db53ecd2ae81fb89c0a6998dec3fece078
Piter <piter@nowhere.net>
2023-01-17 00:16:14 +0100

    dodanie sortowania bąbelkowego

--- main.py
+++ main.py
@@ -3,9 +3,21 @@ import random
 # ZAKRES_MIN i ZAKRES_MAX oznaczają przedział z którego losujemy
 # ILE_LICZB oznacza ilość liczb, którą losujemy
 ZAKRES_MIN = 0
-ZAKRES_MAX = 100
-ILE_LICZB = 100
+ZAKRES_MAX = 1000000
+ILE_LICZB = 10000
 
+def bubelSort(listaLiczb):
+
+    zamiana = True
+    while (zamiana):
+        # optymistycznie
+        zamiana = False
+        for i in range(len(listaLiczb)-1):
+            if listaLiczb[i] > listaLiczb[i+1]:
+                zamiana = True
+                listaLiczb[i], listaLiczb[i+1] = listaLiczb[i+1], listaLiczb[i]
+
+    return listaLiczb
 def generujLiczbyLosowe():
     listaLiczb = []
 
@@ -17,7 +29,9 @@ def generujLiczbyLosowe():
 def main():
     listaLiczb = generujLiczbyLosowe()
     print(listaLiczb)
-
+    posortowane = bubelSort(listaLiczb)
+    print()
+    print(posortowane)
     return
 
 if __name__ == "__main__":
