14ab8aa2e55e99c2659715d9f129a2f5ae508186
Piter <piter@nowhere.net>
2023-01-18 00:06:12 +0100

    dodano algorytm sortowania szybkiego

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import random, datetime
 # ZAKRES_MIN i ZAKRES_MAX oznaczają przedział z którego losujemy
 # ILE_LICZB oznacza ilość liczb, którą losujemy
 ZAKRES_MIN = 0
-ZAKRES_MAX = 1000000
-ILE_LICZB = 1000
+ZAKRES_MAX = 100
+ILE_LICZB = 10000
 
 # generowanie tablicy (listy) liczb losowych w zadanym zakresie i ilości
 def generujLiczbyLosowe():
@@ -18,7 +18,6 @@ def generujLiczbyLosowe():
 
 # sortowanie bąbelkowe
 def bubelSort(listaLiczb):
-
     zamiana = True
     while (zamiana):
         # optymistycznie
@@ -27,6 +26,40 @@ def bubelSort(listaLiczb):
             if listaLiczb[i] > listaLiczb[i+1]:
                 zamiana = True
                 listaLiczb[i], listaLiczb[i+1] = listaLiczb[i+1], listaLiczb[i]
+
+    return listaLiczb
+
+
+
+def quickSort(listaLiczb, lewy_indeks, prawy_indeks):
+    i = lewy_indeks
+    j = prawy_indeks
+    # punkt podziału, tzw. pivot
+    pivot = listaLiczb[(lewy_indeks + prawy_indeks) // 2]
+
+    while True:
+        # szukaj elementu większego niż pivot po lewej stronie tablicy
+        while listaLiczb[i] < pivot:
+            # przesuń indeks
+            i += 1
+        # szukaj elemetnu mniejszego niż pivot po prawej stronie tablicy
+        while listaLiczb[j] > pivot:
+            # przesuń indeks
+            j -= 1
+        # jeśli liczniki się nie minęły, wykonaj zamianę - jednocześnie załatwia
+        # warunek wyjścia przy jednoelementowej tablicy po podziale
+        if i <= j:
+            # zamień liczby
+            listaLiczb[i], listaLiczb[j] = listaLiczb[j], listaLiczb[i]
+            i += 1
+            j -= 1
+        else:
+            break
+    # wywołanie rekurencyjne dla obu przedziałów
+    if lewy_indeks < j:
+        quickSort(listaLiczb, lewy_indeks, j)
+    if prawy_indeks > i:
+        quickSort(listaLiczb, i, prawy_indeks)
 
     return listaLiczb
 
@@ -39,14 +72,26 @@ def main():
     print(listaLiczb)
 
     # sortowanie bąbelkowe
+    listaBubel = listaLiczb.copy()
+    print("Sortowanie bąbelkowe")
     czas_start = datetime.datetime.now()
-    posortowane = bubelSort(listaLiczb)
+    posortowane = bubelSort(listaBubel)
     czas_stop = datetime.datetime.now()
     wyniki['Sortowanie bąbelkowe'] = czas_stop - czas_start
 
     print()
     print(posortowane)
 
+    # sortowanie szybkie
+    listaQuick = listaLiczb.copy()
+    print("Sortowanie szybkie")
+    czas_start = datetime.datetime.now()
+    posortowane = quickSort(listaQuick, 0, len(listaQuick) - 1)
+    czas_stop = datetime.datetime.now()
+    wyniki['Sortowanie szybkie'] = czas_stop - czas_start
+
+    print()
+    print(posortowane)
 
     # wyświetlenie wyników - na razie dirty
     print(wyniki)
